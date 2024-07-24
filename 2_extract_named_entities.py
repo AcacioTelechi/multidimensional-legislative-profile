374c6e654f6d5044712212011258193007c6a639
AcacioTelechi <acaciotelechi@gmail.com>
2024-07-23 21:47:14 -0300

    fix: parallelism

--- 2_extract_named_entities.py
+++ 2_extract_named_entities.py
@@ -4,46 +4,57 @@ from tqdm import tqdm
 from dotenv import load_dotenv
 from utils import utils
 import concurrent.futures
+import pandas as pd
+import torch
+import json
+
 
 load_dotenv()
 
 PROPS_FOLDER = os.getenv("PROPS_FOLDER")
 NER_FOLDER = os.getenv("NER_FOLDER")
+DF_FOLDER = "./output/df_props.csv"
+
 
 def main():
-    seen_props = set(os.listdir(NER_FOLDER))
-
     model = SpanMarkerModel.from_pretrained(
         "lxyuan/span-marker-bert-base-multilingual-uncased-multinerd",
         language="pt",
+        show_progress_bar=False,
     )
-    model.try_cuda()  # Assuming you have a GPU
 
-    def process_prop(prop):  
-        if prop in seen_props:
+    if torch.cuda.is_available():
+        model.to("cuda")
+        print(f"Using CUDA for processing on device {torch.cuda.get_device_name()}")
+    else:
+        print("CUDA not available, using CPU")
+
+    df_props = pd.read_csv(DF_FOLDER)
+    df_props_ementa_not_null = df_props[~df_props["ementa"].isnull()].copy()
+
+    def process_row(row):
+        ementa = row["ementa"]
+
+        try:
+            ner = model.predict(ementa)
+            return {"id_prop": row["id"], "ner": ner}
+        except Exception as e:
+            print(f"Error during prediction for row {row['id']}: {e}")
             return
-        
-        prop_data = utils.open_json(PROPS_FOLDER, prop)
-        ementa = prop_data[0].get("ementa")
 
-        if not ementa:
-            return
+    # Parallel processing with detailed error handling
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(
+            tqdm(
+                executor.map(
+                    process_row, [row for _, row in df_props_ementa_not_null.iterrows()]
+                ),
+                total=len(df_props_ementa_not_null),
+            )
+        )
 
-        ner = model.predict(ementa)
-        utils.save_json(NER_FOLDER, prop.split('.')[0], ner)
-
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        props_to_process = [prop for prop in os.listdir(PROPS_FOLDER) if prop not in seen_props]
-        with tqdm(total=len(props_to_process)) as pbar:  # Initialize the progress bar
-            futures = {executor.submit(process_prop, prop): prop for prop in props_to_process}
-            for future in concurrent.futures.as_completed(futures):
-                # Catch any exceptions raised in the process_prop function
-                try:
-                    future.result()  
-                except Exception as e:
-                    print(f"Error processing {futures[future]}: {e}")
-                pbar.update(1)  # Update the progress bar after each task completes
+    utils.save_json(NER_FOLDER, "NER", results)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
